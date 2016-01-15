bb30636f1caea2b1e8b108c46d1e57dad04d7fa9
Brian Tice <briantice@dhcp-18-111-22-194.dyn.MIT.EDU>
2016-01-15 16:48:03 -0500

    Added a Three over Three beat to the PolyRhythm

--- public/examples/polyrhythm.js
+++ public/examples/polyrhythm.js
@@ -2,24 +2,23 @@
 // Last Modified 1/15/2016
 // Brian Tice
 
-
 var frame = 0;
 var divisionsPerMeasure = 24;
 var currentRow = 0;
 
 var pin = 0;
 
+var pinTwo = 0;
+
 return function() {                 // this function will run at each frame
 
-    /*xForm.setPinHeight(pin,1);
-    pin++;
+   
+    // Four Over Four Time Signature 
     
-    if(pin == 575) {
-        pin = 0;
-    }*/
     
     currentRow = frame % divisionsPerMeasure;
     pin = currentRow * 24;
+    pinTwo = currentRow * 24 + 5;
     
     if( currentRow == 0 ) {
         xForm.setPinHeight(pin,1);
@@ -27,22 +26,37 @@ return function() {                 // this function will run at each frame
     }
     else if( currentRow == 6) {
         xForm.setPinHeight(pin,1);
-        xForm.setPinHeight(pin-144,0);
+        xForm.setPinHeight(pin - 144,0);
     }
     else if ( currentRow == 12 ) {
         xForm.setPinHeight(pin,1);
-        xForm.setPinHeight(pin-144,0);
+        xForm.setPinHeight(pin - 144,0);
     }
     else if ( currentRow == 18 ) {
         xForm.setPinHeight(pin,1);
-        xForm.setPinHeight(pin-144,0);
+        xForm.setPinHeight(pin - 144,0);
     }
+    
+    // Three Over Three Time Signature
+    
+    if( currentRow == 0 ) {
+        xForm.setPinHeight(pinTwo,1);
+        xForm.setPinHeight(pinTwo + 384,0);
+    }
+    else if( currentRow == 8 ){
+        xForm.setPinHeight(pinTwo,1);
+        xForm.setPinHeight(pinTwo - 192,0);
+    }
+    else if( currentRow == 16 ) {
+        xForm.setPinHeight(pinTwo,1);
+        xForm.setPinHeight(pinTwo - 192,0);
+    }
+    
     
     frame++;
     
     
-    // xForm.setPinHeight(24,1);
-    // xForm.setPinHeight(24+23,1);
+    
     
     
 }
